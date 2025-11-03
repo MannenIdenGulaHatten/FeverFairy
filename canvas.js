950882368d2d2de66b0729b6cda05dc5c0a5cf83
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2025-11-03 13:44:54 +0100

    made constructor class monsterImage

--- canvas.js
+++ canvas.js
@@ -1,2 +1,13 @@
 const testCanvas = document.getElementById("testCanvas")
 const ctx = canvas.getContext("2d")
+
+class monsterImage{
+    constructor(x,y,width,height,img) {
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.img=img;
+    }
+}
+
