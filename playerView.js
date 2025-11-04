02a2be78f2225a5492484e3b223812e71146c510
Samir <samirdevs.69420@gmail.com>
2025-11-04 09:53:56 +0100

    lerp clamp

--- playerView.js
+++ playerView.js
@@ -4,14 +4,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
     function clamp(num, min, max) {
         return Math.max(Math.min(num, max), min)
     }
+    function lerp(x, y, a) {
+        return x * (1 - a) + y * a;
+    }
 
     document.addEventListener('mousemove', function(event) {
         const x = event.clientX;
         const y = event.clientY;
-        const width = window.innerWidth
-        const height = window.innerHeight
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const style = window.getComputedStyle(follower);
+        const left = parseFloat(style.left);
+        const top = parseFloat(style.top);
 
-        follower.style.left = `${clamp(x, height/4, width - height/4)}px`;
-        follower.style.top = `${clamp(y, height/4, height - height/4)}px`;
+        if (x > left + 80 || x < left - 80 || y > top + 80 || y < top - 80) {
+            follower.style.left = `${lerp(left, clamp(x, height/4, width - height/4), 0.1)}px`;
+            follower.style.top = `${lerp(top, clamp(y, height/4, height - height/4), 0.1)}px`;
+        }; 
     });
 });
