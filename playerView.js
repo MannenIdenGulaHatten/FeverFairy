3d75e995276efe064f8c173b19da4bdf5c22f595
Samir <samirdevs.69420@gmail.com>
2025-11-04 10:21:07 +0100

    update circel fix

--- playerView.js
+++ playerView.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const follower = document.querySelector('.follower');
+    let mouseX = 0;
+    let mouseY = 0;
 
     function clamp(num, min, max) {
         return Math.max(Math.min(num, max), min)
@@ -8,18 +10,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
         return x * (1 - a) + y * a;
     }
 
-    document.addEventListener('mousemove', function(event) {
-        const x = event.clientX;
-        const y = event.clientY;
+    function updateCirclePos() {
         const width = window.innerWidth;
         const height = window.innerHeight;
         const style = window.getComputedStyle(follower);
         const left = parseFloat(style.left);
         const top = parseFloat(style.top);
+        const leave = height / 5;
 
-        if (x > left + 80 || x < left - 80 || y > top + 80 || y < top - 80) {
-            follower.style.left = `${lerp(left, clamp(x, height/4, width - height/4), 0.1)}px`;
-            follower.style.top = `${lerp(top, clamp(y, height/4, height - height/4), 0.1)}px`;
+        if (mouseX > left + leave || mouseX < left - leave || mouseY > top + leave || mouseY < top - leave) {
+            follower.style.left = `${lerp(left, clamp(mouseX, height/4, width - height/4), 0.05)}px`;
+            follower.style.top = `${lerp(top, clamp(mouseY, height/4, height - height/4), 0.05)}px`;
         }; 
+    }
+
+    document.addEventListener('mousemove', function(event) {
+        const x = event.clientX;
+        const y = event.clientY;
+        
+        mouseX = x;
+        mouseY = y;
     });
+
+    setInterval(updateCirclePos, 16);
 });
