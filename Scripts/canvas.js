daf2fb32dab7d168efea032369315d45ce0ee568
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2025-11-06 14:23:24 +0100

    bufixes

--- Scripts/canvas.js
+++ Scripts/canvas.js
@@ -8,16 +8,16 @@ const img = new Image();
 let room = "Kitchen"
 img.src = 'Images/GameOn' + room + '.png'; // backround ima ge
 
-const door1 = new Image();
+const sten = new Image();
+sten.src = 'Images/BollTEST3 mindre.png'; // stone
+
+/*const door1 = new Image();
 door1.src = 'Images/Door1.png'; // door image
 const door2 = new Image();
-door2.src = 'Images/Door1.png'; // door image
-const testBanana = new Image(); // test image
-testBanana.src = 'Images/testbanan-removebg-preview.png';
+door2.src = 'Images/Door1.png'; // door image */
 
-let mouseX = canvas.width / 2; // gör så att "ljuset" börjar i mitten av skärmen
-let mouseY = canvas.height / 2;
-
+let mouseX = canvas.width / 2;
+let mouseY = canvas.width / 2; // makes the light start position at the center of the screen
 // flashlight circle size
 const radius = 80;
 // how much the room "moves" when you move the cursor
@@ -51,6 +51,7 @@ let currentY = mouseY;
 document.addEventListener('mousemove', (e) => {
     mouseX = e.clientX;
     mouseY = e.clientY;
+    console.log (mouseX + ' ' + mouseY)
 });
 
 function draw() {
@@ -60,8 +61,14 @@ function draw() {
     currentX = lerp(currentX, clamp(mouseX, 0, canvas.width), 0.5);
     currentY = lerp(currentY, clamp(mouseY, 0, canvas.height), 0.5);
     //paralax effect
-    const backgroundgOffsetX = (mouseX / canvas.width - 0.5) * maxShiftX;
+    const backgroundgOffsetX = (mouseX / canvas.width - 0.5) * maxShiftX; 
     const backroundgOffsetY = (mouseY / canvas.height - 0.5) * maxShiftY;
+
+
+    const stenX =600;
+    const stenY =300;
+    const stenOffsetX = (mouseX / canvas.width - 0.5) * maxShiftX; // paralax effect for testrock
+    const stenOffsetY = (mouseY / canvas.height - 0.5) * maxShiftY; 
 
     // dark background / who turned of the lights?
     ctx.fillStyle = 'black';
@@ -75,13 +82,14 @@ function draw() {
 
     ctx.drawImage(img, -backgroundgOffsetX, -backroundgOffsetY, canvas.width, canvas.height);
 
-    let door1Size = getImgScaled(door1.naturalWidth, door1.naturalHeight);
+    ctx.drawImage(sten, stenX - stenOffsetX, stenY - stenOffsetY, 50, 50);
+
+
+    /*let door1Size = getImgScaled(door1.naturalWidth, door1.naturalHeight);
     ctx.drawImage(door1, -backgroundgOffsetX, -backroundgOffsetY, door1Size.X, door1Size.Y)
     let door2Size = getImgScaled(door2.naturalWidth, door2.naturalHeight);
     ctx.drawImage(door2, -backgroundgOffsetX, -backroundgOffsetY, door2Size.X, door2Size.Y)
-    let testBananaSize = getImgScaled(testBanana.naturalWidth, testBanana.naturalHeight);
-    ctx.drawImage(testBanana -backgroundgOffsetX, -backroundgOffsetY, testBananaSize.X, testBananaSize.Y)
-    
+    */
     ctx.fillStyle = 'rgba(0, 0, 255, 0.1)'; //  gives the light a red color light with 10 % oppacity
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
@@ -101,7 +109,7 @@ window.addEventListener('click', function(event) {
     const x = event.clientX;
     const y = event.clientY;
 
-    if (room == "Kitchen" && context.isPointInPath(inBath, x, y)) {
+    if (room == "Kitchen" && ctx.isPointInPath(inBath, x, y)) {
         room = "Bathroom"
         img.src = 'Images/GameOn' + room + '.png';
     }
