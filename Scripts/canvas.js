b5b5cdf62825699d47255e731ab18f6c18ee1547
AleksanderSudol <aleksander.sudol@elev.ga.ntig.se>
2025-11-06 09:53:24 +0100

    s

--- Scripts/canvas.js
+++ Scripts/canvas.js
@@ -1,42 +1,106 @@
 const canvas = document.getElementById('kitchenCanvas');
 const ctx = canvas.getContext('2d');
 
-// Load the image
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const img = new Image();
-img.src = 'BollTEST3 mindre.png';
+let room = "Kitchen"
+img.src = 'Images/GameOn' + room + '.png'; // backround ima ge
 
-// Position and size of the image
-let imgX = 800;
-let imgY = 840;
-let imgWidth = 200;
-let imgHeight = 150;
+const door1 = new Image();
+door1.src = 'Images/Door1.png'; // door image
+const door2 = new Image();
+door2.src = 'Images/Door1.png'; // door image
 
-//  to use later when color is certain colors
-let imageVisible = true;
+let mouseX = canvas.width / 2; // gör så att "ljuset" börjar i mitten av skärmen
+let mouseY = canvas.height / 2;
 
-img.onload = function() {
-  draw();
+// flashlight circle size
+const radius = 80;
+// how much the room "moves" when you move the cursor
+const maxShiftX = 200;
+const maxShiftY = 200;
+
+// game variables (set when select difficulty)
+let fever = 0;
+let maxFever = 67;
+let difficulty = "None";
+let objectsFound = 0;
+let colorFreq = 440; // red: 440, green: 565, blue: 645 THz
+
+
+// clamp and lerp functions stolen from samir aswell as some other stuff but what it does is make giveen max and minimum so that the mouse / light dosent go outside the screen)
+function clamp(num, min, max) {
+    return Math.max(Math.min(num, max), min);
+}
+function lerp(x, y, a) {
+    return x * (1 - a) + y * a;
+}
+function getImgScaled(x, y) {
+    const scaleX = x * canvas.width / 2880;
+    const scaleY = y * canvas.height / 1620;
+    return {X: scaleX, Y: scaleY};
 }
 
-// Draw it function
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas 
-  if (imageVisible) {
-    ctx.drawImage(img, imgX, imgY, imgWidth, imgHeight);
-  }
-}
-
-// when you click
-canvas.addEventListener('click', function(e) {
-  const rect = canvas.getBoundingClientRect();
-  const mouseX = e.clientX - rect.left;
-  const mouseY = e.clientY - rect.top;
-
-  // if click is inside the image 
-  if (imageVisible &&
-      mouseX >= imgX && mouseX <= imgX + imgWidth &&
-      mouseY >= imgY && mouseY <= imgY + imgHeight) {
-    imageVisible = false; // hide the image
-    draw(); // redraw canvas
-  }
+let currentX = mouseX; //circle at mouse position
+let currentY = mouseY;
+// takes mouse position when move mouse
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 });
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // circle / lighft follows mouse
+    currentX = lerp(currentX, clamp(mouseX, 0, canvas.width), 0.5);
+    currentY = lerp(currentY, clamp(mouseY, 0, canvas.height), 0.5);
+    //paralax effect
+    const backgroundgOffsetX = (mouseX / canvas.width - 0.5) * maxShiftX;
+    const backroundgOffsetY = (mouseY / canvas.height - 0.5) * maxShiftY;
+
+    // dark background / who turned of the lights?
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // cuts out a circle that is the flashlight light with ctx.clip()
+    ctx.save(); 
+    ctx.beginPath();
+    ctx.arc(currentX, currentY, radius, 0, Math.PI * 2); 
+    ctx.clip();
+
+    ctx.drawImage(img, -backgroundgOffsetX, -backroundgOffsetY, canvas.width, canvas.height);
+
+    let door1Size = getImgScaled(door1.naturalWidth, door1.naturalHeight);
+    ctx.drawImage(door1, -backgroundgOffsetX, -backroundgOffsetY, door1Size.X, door1Size.Y)
+    let door2Size = getImgScaled(door2.naturalWidth, door2.naturalHeight);
+    ctx.drawImage(door2, -backgroundgOffsetX, -backroundgOffsetY, door2Size.X, door2Size.Y)
+    
+    ctx.fillStyle = 'rgba(0, 0, 255, 0.1)'; //  gives the light a red color light with 10 % oppacity
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.restore();
+
+    requestAnimationFrame(draw);
+}
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+window.addEventListener('click', function(event) {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const x = event.clientX;
+    const y = event.clientY;
+
+    if (room == "Kitchen" && context.isPointInPath(inBath, x, y)) {
+        room = "Bathroom"
+        img.src = 'Images/GameOn' + room + '.png';
+    }
+});
+
+img.onload = draw;
--- Scripts/playerView.js
+++ Scripts/playerView.js
@@ -1,8 +1,11 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const follower = document.querySelector('.follower');
     const background = document.querySelector('.background');
+    const jeffer = document.querySelector('.jeffer');
+
     let mouseX = 0;
     let mouseY = 0;
+    let mouseChanged = false;
 
     function clamp(num, min, max) {
         return Math.max(Math.min(num, max), min)
@@ -14,29 +17,60 @@ window.addEventListener("DOMContentLoaded", (event) => {
     function updatePositions() {
         const width = window.innerWidth;
         const height = window.innerHeight;
+        
         const circleStyle = window.getComputedStyle(follower);
-        const cLeft = parseFloat(circleStyle.left);
-        const cTop = parseFloat(circleStyle.top);
-        const leave = height / 5;
+        let cLeft = parseFloat(circleStyle.left);
+        let cTop = parseFloat(circleStyle.top);
+        const leave = height / 6;
+
         const bgStyle = window.getComputedStyle(background);
-        const bgLeft = parseFloat(bgStyle.left);
-        const bgTop = parseFloat(bgStyle.top);
+        let bgLeft = parseFloat(bgStyle.left);
+        let bgTop = parseFloat(bgStyle.top);
+        const widthR = width * 0.5625;
+        const rDiff = 0.5625 * (width / height);
 
-        if (mouseX > cLeft + leave || mouseX < cLeft - leave || mouseY > cTop + leave || mouseY < cTop - leave) {
-            follower.style.left = `${lerp(cLeft, clamp(mouseX, height/4, width - height/4), 0.05)}px`;
-            follower.style.top = `${lerp(cTop, clamp(mouseY, height/4, height - height/4), 0.05)}px`;
+        if (mouseChanged == false || mouseX > cLeft + leave || mouseX < cLeft - leave || mouseY > cTop + leave || mouseY < cTop - leave) {
+            const radius = parseFloat(circleStyle.width) / 2; 
 
-            background.style.left = `${lerp(bgLeft, clamp((width - mouseX * 1.5) - width/0.75, -width*1, width*1), 0.05)}px`;
-            background.style.top = `${lerp(bgTop, clamp((height - mouseY * 1.5) - height/0.75, -height*1.5, height*1), 0.05)}px`;
-        }; 
+            cLeft = lerp(cLeft, clamp(mouseX, height / 4, width - height / 4), 0.05);
+            cTop = lerp(cTop, clamp(mouseY, height / 4, height - height / 4), 0.05);
+
+            follower.style.left = `${cLeft}px`;
+            follower.style.top = `${cTop}px`;
+
+            jeffer.style.setProperty('--x', `${cLeft}px`);
+            jeffer.style.setProperty('--y', `${cTop}px`);
+            jeffer.style.setProperty('--radius', `${radius}px`);
+
+            bgLeft = lerp(bgLeft, clamp((width * 1.5 - mouseX * 1.5) - width / 0.75, -width * 1.25, 0), 0.05);
+            bgTop = lerp(bgTop, clamp((-mouseY * rDiff * 1.75), -widthR * 2.5, -widthR * 0.5), 0.05);
+
+            background.style.left = `${bgLeft}px`;
+            background.style.top = `${bgTop}px`;
+
+            mouseChanged = false;
+        };
     };
 
     document.addEventListener('mousemove', function(event) {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
         const x = event.clientX;
         const y = event.clientY;
-        
-        mouseX = x;
-        mouseY = y;
+
+        mouseX = clamp(x, 0, width);
+        mouseY = clamp(y, 0, height);
+
+        mouseChanged = true;
+    });
+
+    document.addEventListener('click', function(event) {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const x = event.clientX;
+        const y = event.clientY;
+
+        console.log(x, y)
     });
 
     setInterval(updatePositions, 16);
