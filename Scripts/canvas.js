2b82d642a45b19e79f7dbba06305264861d7743b
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2025-11-06 13:57:59 +0100

    fixa paralax för test stenen, kan fixa åt andra objekt lätt

--- Scripts/canvas.js
+++ Scripts/canvas.js
@@ -11,18 +11,16 @@ img.src = 'Images/GameOn' + room + '.png'; // backround ima ge
 const sten = new Image();
 sten.src = 'Images/BollTEST3 mindre.png'; // stone
 
-const door1 = new Image();
+/*const door1 = new Image();
 door1.src = 'Images/Door1.png'; // door image
 const door2 = new Image();
-door2.src = 'Images/Door1.png'; // door image
-const testBanana = new Image(); // test image
-testBanana.src = 'Images/testbanan-removebg-preview.png';
+door2.src = 'Images/Door1.png'; // door image */
 
 let mouseX = canvas.width / 2; // gör så att "ljuset" börjar i mitten av skärmen
 let mouseY = canvas.height / 2;
 
 // flashlight circle size
-const radius = 80;
+const radius = 800;
 // how much the room "moves" when you move the cursor
 const maxShiftX = 200;
 const maxShiftY = 200;
@@ -67,6 +65,12 @@ function draw() {
     const backgroundgOffsetX = (mouseX / canvas.width - 0.5) * maxShiftX;
     const backroundgOffsetY = (mouseY / canvas.height - 0.5) * maxShiftY;
 
+
+    const stenX =600;
+    const stenY =300;
+    const stenOffsetX = (mouseX / canvas.width - 0.5) * maxShiftX; //fungerar inte häller. tror för att den placerar den i mitten av skärmen. uh
+    const stenOffsetY = (mouseY / canvas.height - 0.5) * maxShiftY; // 277, 327, 50, 50
+
     // dark background / who turned of the lights?
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -78,14 +82,15 @@ function draw() {
     ctx.clip();
 
     ctx.drawImage(img, -backgroundgOffsetX, -backroundgOffsetY, canvas.width, canvas.height);
-    ctx.drawImage(sten,277, 327, 50, 50);
-    let door1Size = getImgScaled(door1.naturalWidth, door1.naturalHeight);
+
+    ctx.drawImage(sten, stenX - stenOffsetX, stenY - stenOffsetY, 50, 50);
+
+
+    /*let door1Size = getImgScaled(door1.naturalWidth, door1.naturalHeight);
     ctx.drawImage(door1, -backgroundgOffsetX, -backroundgOffsetY, door1Size.X, door1Size.Y)
     let door2Size = getImgScaled(door2.naturalWidth, door2.naturalHeight);
     ctx.drawImage(door2, -backgroundgOffsetX, -backroundgOffsetY, door2Size.X, door2Size.Y)
-    let testBananaSize = getImgScaled(testBanana.naturalWidth, testBanana.naturalHeight);
-    ctx.drawImage(testBanana -backgroundgOffsetX, -backroundgOffsetY, testBananaSize.X, testBananaSize.Y)
-    
+    */
     ctx.fillStyle = 'rgba(0, 0, 255, 0.1)'; //  gives the light a red color light with 10 % oppacity
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
