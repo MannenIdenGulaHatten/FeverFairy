78afe440e06f2340e98702ec8e0e0a53189edf19
AleksanderSudol <aleksander.sudol@elev.ga.ntig.se>
2025-11-12 13:29:30 +0100

    credits

--- Scripts/creditscanvas.js
+++ Scripts/creditscanvas.js
@@ -2,10 +2,10 @@ const canvas = document.getElementById('creditscanvas');
 const ctx = canvas.getContext('2d');
 
 const img = new Image();
-img.src = 'images/credits_person2.png';
+img.src = 'images/credits_org2.png';
 
 const img2 = new Image();
-img2.src = 'images/credits_sound4.png';
+img2.src = 'images/credits_hover2.png';
 
 
 
@@ -17,32 +17,24 @@ canvas.addEventListener('mousemove', (event) => {
   const hoverX = event.clientX - rect.left;
   const hoverY = event.clientY - rect.top;
   console.log(hoverX+' '+hoverY);
+      if (hoverX >= 20 && hoverX <= 145 && hoverY >= 10 && hoverY <= 105) {
+        currentImg = img2; 
+        } else {
+        currentImg = img; 
+        }  
+});
 
-  if (hoverX >= 530 && hoverX <= 730 && hoverY >= 230 && hoverY <= 290) {
-      currentImg = img2; 
-    } else if (hoverX >= 530 && hoverX <= 730 && hoverY >= 295 && hoverY <= 350) {
-      currentImg = img3; 
-    } else if (hoverX >= 530 && hoverX <= 730 && hoverY >= 355 && hoverY <= 400) {
-      currentImg = img4; 
-    } else {
-      currentImg = img; 
-    }
-  });
 const sound = new Audio('click3.ogg');//https://gamesounds.xyz/Kenney%27s%20Sound%20Pack/UI%20Audio/click3.ogg
 canvas.addEventListener('click', (event) => {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-    
-  if (x >= 530 && x <= 730) {
-    sound.play()
-    .then(() => console.log('Ljudet spelas upp!'))
-    .catch(err => console.error('Kunde inte spela upp ljudet:', err));
-    if (y >= 230 && y <= 290)
-      location.replace("/feverfairy/levelselect.html");
-    else if (y >= 295 && y <= 350)
-      location.replace("/feverfairy/credits.html");
-  }
+    if (x >= 20 && x <= 145 && y >= 10 && y <= 105) {
+        location.replace("/feverfairy/index.html")
+        currentImg = img2; 
+        } else {
+        currentImg = img; 
+        }  
 });
 
 function draw() {
